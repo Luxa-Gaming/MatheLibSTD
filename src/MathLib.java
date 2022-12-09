4588b8dbc2582bd091f173d26e215bf4cb547aa5
jmarquar <jaron.marquardt@materna.de>
2022-12-09 14:03:26 +0100

    - Fakultät

--- src/MathLib.java
+++ src/MathLib.java
@@ -34,4 +34,12 @@ public class MathLib {
         return a;
     }
 
+    public static int fakultaet(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * fakultaet(n - 1);
+        }
+
+    }
 }
